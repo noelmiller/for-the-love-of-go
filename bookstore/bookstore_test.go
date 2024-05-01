db6f26fc098a1f644fa5cf173cf0954a1a3e291d
Noel Miller <noelmiller@protonmail.com>
2024-05-01 16:51:29 -0500

    finished chapter 5

---
+++ bookstore/bookstore_test.go
@@ -0,0 +1,46 @@
+package bookstore_test
+
+import (
+	"bookstore"
+	"testing"
+)
+
+func TestBook(t *testing.T) {
+	t.Parallel()
+	_ = bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+}
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	book := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+	want := 1
+	result, err := bookstore.Buy(book)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	book := bookstore.Book{
+		Title:  "Spart Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(book)
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
+	}
+}
