0c77e565526972b79c435f467f3e7aee6c6f3c5a
Noel Miller <noelmiller@protonmail.com>
2024-05-06 09:58:23 -0500

    finished chapter 9

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -1,7 +1,16 @@
 package mytypes
 
+import "strings"
+
 type MyInt int
 type MyString string
+type MyBuilder struct {
+	Contents strings.Builder
+}
+
+type StringUppercaser struct {
+	Contents strings.Builder
+}
 
 // Twice multiplies its receiver by 2 and returns
 // the result.
@@ -11,4 +20,19 @@ func (i MyInt) Twice() MyInt {
 
 func (s MyString) MyStringLen() int {
 	return len(s)
+}
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+func (su StringUppercaser) ToUpper() string {
+	return strings.ToUpper(su.Contents.String())
+}
+
+func Double(input *int) {
+	*input *= 2
+}
+
+func (input *MyInt) Double() {
+	*input *= 2
 }
--- mytypes/mytypes_test.go
+++ mytypes/mytypes_test.go
@@ -2,6 +2,7 @@ package mytypes_test
 
 import (
 	"mytypes"
+	"strings"
 	"testing"
 )
 
@@ -22,5 +23,71 @@ func TestMyStringLen(t *testing.T) {
 	got := input.MyStringLen()
 	if want != got {
 		t.Errorf("length of %s: want %d, got %d", input, want, got)
+	}
+}
+
+func TestStringsBuilder(t *testing.T) {
+	t.Parallel()
+	var sb strings.Builder
+	sb.WriteString("Hello, ")
+	sb.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := sb.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+	wantLen := 15
+	gotLen := sb.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q: want len %d, got %d", sb.String(), wantLen, gotLen)
+	}
+}
+
+func TestMyBuilderHello(t *testing.T) {
+	t.Parallel()
+	var sb mytypes.MyBuilder
+	sb.Contents.WriteString("Hello, ")
+	sb.Contents.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := sb.Contents.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+	wantLen := 15
+	gotLen := sb.Contents.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q: want len %d, got %d", sb.Contents.String(), wantLen, gotLen)
+	}
+}
+
+func TestStringUppercaser(t *testing.T) {
+	t.Parallel()
+	var su mytypes.StringUppercaser
+	su.Contents.WriteString("Hello, Gophers!")
+	want := "HELLO, GOPHERS!"
+	got := su.ToUpper()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestDouble(t *testing.T) {
+	t.Parallel()
+	var x int = 12
+	want := 24
+	mytypes.Double(&x)
+	if want != x {
+		t.Errorf("want %d, got %d", want, x)
+	}
+}
+
+func TestDoublePointer(t *testing.T) {
+	t.Parallel()
+	x := mytypes.MyInt(12)
+	want := mytypes.MyInt(24)
+	p := &x
+	p.Double()
+	if want != x {
+		t.Errorf("want %d, got %d", want, x)
 	}
 }
